dc40672538a34d54b89e780d1b93cb9e03058283
xyoojlinda <xyoojlinda@users.noreply.github.com>
2021-03-23 00:34:35 -0500

    changed to new github url

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className="github">
       <small>
-        <a href="https://github.com/xyoojlinda/myweatherproject">
+        <a href="https://github.com/xyoojlinda/weather-react-shecodes.git">
           Open-source code{" "}
         </a>
         by Linda Xiong
