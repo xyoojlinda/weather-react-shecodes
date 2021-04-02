d3cd32d2342fecdbad519002d6672caf2c1d2a7e
xyoojlinda <xyoojlinda@users.noreply.github.com>
2021-04-01 22:19:27 -0500

    added weather conversion

--- src/Description.js
+++ src/Description.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Description.css";
 import FormattedDate from "./FormattedDate.js";
 import Icon from "./Icon.js";
 import Forecast from "./Forecast.js";
@@ -7,37 +6,26 @@ import Forecast from "./Forecast.js";
 
 export default function Description(props) {
   return (
-    <div className="DescriptionData">
-      <h1>   <Forecast defaultCity="Miami" /></h1>
-      <ul>
-        <li> <FormattedDate date={props.data.dt} /></li>
-        <li className="text-capitalize"> {props.data.description}</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="clearfix">
-            <span className="float-start">
-              <Icon code={props.data.icon} />
-            </span>
-            <span className="float-start">
-              <strong id="temperature">
-                {props.data.temperature}
-              </strong>
-              <a href="/" className="active">
-                °F
-                </a>{" "}
-                |<a href="/">°C</a>
-            </span>
-          </div>
+    <div className="Description">
+      <h1 className="city"> {props.info.city} </h1>
+      <div className="row d-flex align-items-center">
+        <div className="col-4">
+          <WeatherTemp celsius={props.info.temp} />
+          <p className="description text-capitalize">
+            {props.info.description}
+          </p>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>
-              Humidity:{props.data.humidity}%
-                 </li>
-            <li>
-              Wind speed: {Math.round(props.data.wind)}mph
-                </li>
+        <div className="col-4">
+          <Icon
+            code={props.info.icon}
+            alt={props.info.description}
+          />
+        </div>
+        <div className="col-4">
+          <ul className="DescriptionInfo">
+            <li>Feels like: {props.info.feelsLike}°C</li>
+            <li>Humidity: {props.info.humidity}%</li>
+            <li>Wind: {props.info.windSpeed} km/h</li>
           </ul>
         </div>
       </div>
