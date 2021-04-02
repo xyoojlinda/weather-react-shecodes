ccd3b9b572bb666db6c41f07032afd572e20fb71
xyoojlinda <xyoojlinda@users.noreply.github.com>
2021-04-01 22:23:54 -0500

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,35 @@
+import React, { useState } from "react";
+import FormattedDate from "./FormattedDate";
+import Forecast from "./Forecast";
+import axios from "axios";
+
 import "./Search.css";
+import Description from "./Description";
+
+export default function Search(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      date: new Date(response.data.dt * 1000),
+      city: response.data.name,
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
 
   if (weatherData.ready) {
+    return (
+      <div className="Search">
+        <div className="location">
+          <form className="search" onSubmit={handleSubmit}>
+    );
+  } else {
+    search();
+    return <h1> Loading... </h1>;
+  }
 }
