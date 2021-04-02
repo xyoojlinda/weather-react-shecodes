e2a73afaa6419d0ad8ebc2e2bebdba440bf13e9e
xyoojlinda <xyoojlinda@users.noreply.github.com>
2021-04-02 00:11:25 -0500

    changed name of component

--- src/Icon.js
+++ src/Icon.js
@@ -1,34 +1,35 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
+
+import "./Icon.css";
 
 export default function Icon(props) {
   const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": "PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "PARTLY_CLOUDY_DAY",
-    "03n": "PARTLY_CLOUDY_NIGHT",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10d": "RAIN",
-    "10n": "RAIN",
-    "11d": "RAIN",
-    "11n": "RAIN",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG"
+    "01d": "clear-day",
+    "01n": "clear-night",
+    "02d": "partly-cloudy-day",
+    "02n": "partly-cloudy-night",
+    "03d": "cloudy",
+    "03n": "cloudy",
+    "04d": "cloudy",
+    "04n": "cloudy",
+    "09d": "rain",
+    "09n": "rain",
+    "10d": "partly-cloudy-day-rain",
+    "10n": "partly-cloudy-night-rain",
+    "11d": "thunderstorms",
+    "11n": "thunderstorms",
+    "13d": "partly-cloudy-day-snow",
+    "13n": "partly-cloudy-day-snow",
+    "50d": "mist",
+    "50n": "mist",
   };
 
+  let iconUrl = `https://bmcdn.nl/assets/weather-icons/v1.4.1/${codeMapping[props.code]
+    }.svg`;
+
   return (
-    <ReactAnimatedWeather
-      icon={codeMapping[props.code]}
-      color="	#A9A9A9"
-      size={52}
-      animate={true}
-    />
+    <div className="WeatherIcon">
+      <img className="mainIcon" src={iconUrl} alt={props.alt} />
+    </div>
   );
 }
