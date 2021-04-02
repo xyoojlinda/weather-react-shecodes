ca2ce9d10dcd34f237edad1c1ecc347694641199
xyoojlinda <xyoojlinda@users.noreply.github.com>
2021-04-02 00:22:09 -0500

    Added Forecast

---
+++ src/ForecastElement.js
@@ -0,0 +1,24 @@
+import React from "react";
+import WeatherIcon from "./Icon";
+
+export default function ForecastElement(props) {
+  function hour() {
+    let date = new Date(props.prediction.dt * 1000);
+    let hours = date.getHours();
+    return `${hours}:00`;
+  }
+
+  function temp() {
+    let temperature = Math.round(props.prediction.main.temp);
+    return `${temperature}°C`;
+  }
+  console.log(props.prediction);
+
+  return (
+    <div className="col">
+      {hour()}
+      <Icon code={props.prediction.weather[0].icon} />
+      {temp()}
+    </div>
+  );
+}
